187ba365d5d9dd27a180410c1e17f01f068e58a0
unknown <bongthoi1111@gmail.com>
2018-09-21 16:26:29 +0700

    product API is ok

--- src/services/productService.js
+++ src/services/productService.js
@@ -10,13 +10,14 @@ module.exports = class ProductService {
     constructor() { };
     getAll(req, res, next) {
         let method = "productService/getAll";
-        console.log(method+ "  -->success");
+        console.log(method);
 
 
         productRepo.getAll(function (err, data) {
             if (err) {
                 res.json(err);
             } else {
+                console.log(method+ "  -->success");
                 res.json(data);
             }
         });
@@ -24,13 +25,14 @@ module.exports = class ProductService {
 
     insert(req, res) {
         let method = "productService/insert";
-        console.log(method+ "  -->success");
+        console.log(method);
 
         let product = new Product(null, req.body.title, req.body.description,req.body.quantity, req.body.price, req.body.owner);
         productRepo.insert(product, function (err, data) {
             if (err) {
                 res.json(err);
             } else {
+                console.log(method+ "  -->success");
                 res.json(data);
             }
         });
@@ -39,12 +41,13 @@ module.exports = class ProductService {
 
     getByID(req, res) {
         let method = "productService/getByID: " + req.params.id;
-        console.log(method+ "  -->success");
+        console.log(method);
 
         productRepo.getByID(req.params.id, function (err, data) {
             if (err) {
                 res.json(err);
             } else {
+                console.log(method+ "  -->success");
                 res.json(data);
             }
         });
@@ -53,13 +56,14 @@ module.exports = class ProductService {
 
     update(req, res) {
         let method = "productService/update: " + req.params.id;
-        console.log(method+ "  -->success");
+        console.log(method);
 
         let product = new Product(req.params.id, req.body.title, req.body.description,req.body.quantity,req.body.price, req.body.owner);
         productRepo.update(product, function (err, data) {
             if (err) {
                 res.json(err);
             } else {
+                console.log(method+ "  -->success");
                 res.json(data);
             }
         });
@@ -68,12 +72,13 @@ module.exports = class ProductService {
 
     delete(req, res) {
         let method = "productService/delete: " + req.params.id;
-        console.log(method+ "  -->success");
+        console.log(method);
 
         productRepo.delete(req.params.id, function (err, data) {
             if (err) {
                 res.json(err);
             } else {
+                console.log(method+ "  -->success");
                 res.json(data);
             }
         });
