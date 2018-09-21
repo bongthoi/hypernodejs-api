26b0e5e3303de7a2125b59c7e77f81106171e6a1
unknown <bongthoi1111@gmail.com>
2018-09-21 10:51:46 +0700

    update

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ var api_config=require("./config/api_config.json");
 /******************************** */
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
 
 
 /** */
